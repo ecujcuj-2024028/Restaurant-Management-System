aa436b67b664ed3bda5bc9a33d3d26dbe1d05ee4
ecujcuj-2024028 <ecujcuj-2024028@kinal.edu.gt>
2026-04-29 18:17:33 -0600

    feat: implementar responsive del layout

--- Frontend/src/app/layouts/DashboardPage.jsx
+++ Frontend/src/app/layouts/DashboardPage.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../../shared/components/layout/Sidebar'
 import Navbar from '../../shared/components/layout/Navbar'
 
 const DashboardPage = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
   return (
-    <div className="flex min-h-screen bg-gray-900">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Navbar />
-        <main className="flex-1 p-6">
-          <Outlet />
+    <div className="flex min-h-screen bg-gray-950 overflow-x-hidden">
+      {/* Overlay para móvil */}
+      {isSidebarOpen && (
+        <div 
+          className="fixed inset-0 bg-black/60 backdrop-blur-sm z-40 lg:hidden transition-opacity"
+          onClick={() => setIsSidebarOpen(false)}
+        />
+      )}
+
+      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+      
+      <div className="flex-1 flex flex-col min-w-0">
+        <Navbar onMenuClick={() => setIsSidebarOpen(true)} />
+        <main className="flex-1 p-4 lg:p-8 overflow-y-auto">
+          <div className="max-w-7xl mx-auto">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
