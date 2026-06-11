3129605f183793cae4e0c4d538a0a2a7fdcf9a57
csantos-2024022 <csantos-2024022@kinal.edu.gt>
2026-06-11 16:29:35 -0600

    feat: implementar Gestión de Disponibilidad de Productos (Vista Admin)

---
+++ MobileApp/src/api/products.jsx
@@ -0,0 +1,11 @@
+import api from './api';
+
+export const getAdminProducts = async (restaurantId) => {
+  const response = await api.get(`/products/restaurant/${restaurantId}`);
+  return response.data;
+};
+
+export const updateProductStatus = async (id, isAvailable) => {
+  const response = await api.patch(`/products/${id}/status`, { isAvailable });
+  return response.data;
+};
